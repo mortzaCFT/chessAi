71d49b2cb554b2c0f19526bad64959246ff65021
mortzaCFT <79837660+mortzaCFT@users.noreply.github.com>
2024-02-08 19:36:07 +0330

    Update Board.py

--- Board.py
+++ Board.py
@@ -2,16 +2,14 @@ import chess
 from core import ChessDetector
 
 class Board:
+    
     def __init__(self):
         self.board = chess.Board()
-
-    def initialize_board(self):
-      ChessDetector.set_default_locations(self)
-        # Set up the rest of the pieces...
-        
+        # Set up the default,rest of the pieces...
+        ChessDetector.set_default_locations(self)
+   
     def update_board(self, piece_locations):
         self.board.clear()
-        
         # Map the detected piece locations to squares on the board
         for piece, location in piece_locations.items():
             x, y = location
@@ -24,27 +22,22 @@ class Board:
     def get_chess_piece_type(self, piece):
 
         piece_map = {
-            'king': chess.KING,
-            'queen': chess.QUEEN,
+            'king'   : chess.KING,
+            'queen'  : chess.QUEEN,
             'bishop1': chess.BISHOP,
             'bishop2': chess.BISHOP,
             'knight1': chess.KNIGHT,
             'knight2': chess.KNIGHT,
-            'rook1': chess.ROOK,
-            'rook2': chess.ROOK,
-            'pawn1': chess.PAWN,
-            'pawn2': chess.PAWN,
+            'rook1'  : chess.ROOK,
+            'rook2'  : chess.ROOK,
+            'pawn1'  : chess.PAWN,
+            'pawn2'  : chess.PAWN,
+            'pawn3'  : chess.PAWN,
+            'pawn4'  : chess.PAWN,
+            'pawn5'  : chess.PAWN,
+            'pawn6'  : chess.PAWN,
+            'pawn7'  : chess.PAWN,
+            'pawn8'  : chess.PAWN,
         }
+        #Under development...
         return piece_map.get(piece, chess.PAWN)
-        
-    def get_chess_piece_color(self, piece):
-        if self.player_color == 'white':
-            if piece == 'king' or piece.startswith('rook') or piece.startswith('pawn'):
-                return chess.WHITE
-            else:
-                return chess.BLACK
-        else:
-            if piece == 'king' or piece.startswith('rook') or piece.startswith('pawn'):
-                return chess.BLACK
-            else:
-                return chess.WHITE
